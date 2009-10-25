504f87929e854df8456b660965b702dd212fe281
yoppi <y.hirokazu@gmail.com>
2009-10-25 23:12:57 +0900

    chapter04: Added create_indextables() method

--- chapter04/search_engine.rb
+++ chapter04/search_engine.rb
@@ -1,15 +1,16 @@
 require 'set'
 require 'open-uri'
 require 'rubygems'
-require 'active_record'
 require 'hpricot'
+require 'sqlite3'
 
 class Crawler
-  def initialize
-    
+  def initialize(db)
+    @con = SQLite3::Database.new(db)
   end
 
   def dbcommit
+    @con.commit
   end
 
   def get_entryid
@@ -23,7 +24,7 @@ class Crawler
   end
 
   def add_linkref(url_from, url_to, link_text)
-    
+
   end
 
   def add_toindex(url, hpricot)
@@ -34,10 +35,20 @@ class Crawler
   end
 
   def create_indextables
+    @con.execute("create table urllist(url)")
+    @con.execute("create table wordlist(word)")
+    @con.execute("create table wordlocation(urlid, wordid, location)")
+    @con.execute("create table link(fromid integer, toid integer)")
+    @con.execute("create table linkwords(wordid, linkid)")
+    @con.execute("create index urlidx on urllist(url)")
+    @con.execute("create index wordidx on wordlist(word)")
+    @con.execute("create index wordurlidx on wordlocation(wordid)")
+    @con.execute("create index urltoidx on link(toid)")
+    @con.execute("create index urlfromidx on link(fromid)")
   end
 
   def crawl(pages, depth=2)
-    depth.times do 
+    depth.times do
       new_pages = Set.new
       pages.each do |page|
         begin
@@ -53,13 +64,13 @@ class Crawler
         links.each do |link|
           url = link[:href]
           next unless url
-          
+
           url = URI.join(page, url).to_s
           if url[0...4] == 'http' and !indexed?(url)
             new_pages << url
           end
           link_text = get_text(url)
-          add_linkref(page, url, link_text) 
+          add_linkref(page, url, link_text)
         end
         dbcommit
       end
@@ -69,7 +80,10 @@ class Crawler
 end
 
 if __FILE__ == $0
-  page_list = ["http://kiwitobes.com/wiki/Perl.html"]
-  crawler = Crawler.new
-  crawler.crawl(page_list)
+  #page_list = ["http://kiwitobes.com/wiki/Perl.html"]
+  #crawler = Crawler.new
+  #crawler.crawl(page_list)
+
+  crawler = Crawler.new("searchindex.db")
+  crawler.create_indextables
 end
